f604135296ba40dc191d731c913cff8c9899c716
Gagan Bindal <gaganbindal2002@gmail.com>
2023-08-06 03:37:33 +0530

    Assignment completed

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {  useEffect, useState } from "react";
 
 // Data
 import mockData from "../assets/data.json";
@@ -14,23 +14,81 @@ import List from "../component/list/List";
 import styles from "./Dashboard.module.css";
 import Card from "../component/card/Card";
 
+// Adding debounce for search functionality
+const debounce = (callback, delay) => {
+  let timeoutId;
+  return(...args) =>{
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      callback.apply(null, args);
+    }, delay);
+  };
+};
+
 const Dashboard = () => {
   const [currency, setCurrency] = useState("EUR");
   const [searchText, setSearchText] = useState("");
   const [selectedOrderDetails, setSelectedOrderDetails] = useState({});
   const [selectedOrderTimeStamps, setSelectedOrderTimeStamps] = useState({});
+  const [orderSubmittedVal, setorderSubmittedVal] = useState({});
+  const [searchedRows, setSearchedRows] = useState([]);
+
+  //Defining handleClick to populate the cards above the table
+  const handleClick = (row) => {
+      setSelectedOrderDetails(row.executionDetails)
+      const matchingTimestamp = timestamps.results.find((stamp) => stamp.id === row.id);
+      setSelectedOrderTimeStamps(matchingTimestamp.timestamps || {});
+  }
+
+  //Defining orderVolume to get the whole list of currency in orderVolume object
+  const orderVolume = mockData.results[0].bestExecutionData.orderVolume;
+
+  //This useEffect is defined to get the value of orderSubmitted from timestamp JSON
+  // Split method is used to get the value of order Submitted date
+  useEffect(()=> {
+    const map = {};
+    timestamps.results.forEach((order) => {
+     map[order["&id"]] = order.timestamps.orderSubmitted.split("T")[0];
+    });
+    setorderSubmittedVal(map);
+  }, []);
+
+
+  //Search functionality
+  useEffect(() => {
+    const searchRows = () => {
+      const searchValue = mockData.results.filter((row) => {
+        const orderVolume = row.bestExecutionData.orderVolume[currency];
+        
+        return (
+          //Possible search methods with id, buysellIndicator, orderStatus or using orderVolume also
+          row["&id"].toLowerCase().includes(searchText.toLowerCase()) ||
+          row.executionDetails.buySellIndicator.toLowerCase().includes(searchText.toLowerCase()) ||
+          row.executionDetails.orderStatus.toLowerCase().includes(searchText.toLowerCase()) ||
+          String(orderVolume).includes(searchText)
+        );
+      });
+      setSearchedRows(searchValue);
+    };
+
+    const debounceSearchRows = debounce(searchRows, 200);
+    debounceSearchRows();
+
+    return() => clearTimeout(debounceSearchRows);
+  }, [searchText,currency]);
 
   return (
     <div>
       <div className={styles.header}>
-        <HeaderTitle primaryTitle="Orders" secondaryTitle="5 orders" />
+        <HeaderTitle primaryTitle="Orders" secondaryTitle={`${searchedRows.length} orders`} />
         <div className={styles.actionBox}>
           <Search
             value={searchText}
             onChange={(e) => setSearchText(e.target.value)}
+            
           />
           <Dropdown
-            options={["GBP", "USD", "JPY", "EUR"]}
+            options={Object.keys(orderVolume)}
             onChange={(e) => setCurrency(e.target.value)}
             selectedItem={currency}
           />
@@ -47,7 +105,11 @@ const Dashboard = () => {
             title="Selected Order Timestamps"
           />
         </div>
-        <List rows={mockData.results} />
+        <List rows={searchedRows} 
+        onClick={handleClick} 
+        currency={currency}
+        orderSubmittedVal={orderSubmittedVal}
+         />
       </div>
     </div>
   );
